9a5d9e32c26663a797d23bf38ca50522aaa9e573
jonsalkin <50082962+jonsalkin@users.noreply.github.com>
2023-05-15 18:09:43 -0400

    Initial Commit

---
+++ APIDataInTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIDataInTableView
+//
+//  Created by Jon Salkin on 5/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
